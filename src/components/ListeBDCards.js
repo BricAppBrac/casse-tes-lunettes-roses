6a0380cea2d124f5ca929373e53a1615f494f267
BricAppBrac <sandrinesteiger3000@gmail.com>
2024-01-24 09:56:23 +0100

    V1.3 4ème publication

--- src/components/ListeBDCards.js
+++ src/components/ListeBDCards.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 
 const ListeBDCards = () => {
-  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const [imgIndices, setImgIndices] = useState({
+    coeurVegan: 0,
+    cochons: 0,
+  });
+
+  const renderStars = () => (
+    <h3>
+      {[...Array(5)].map((_, i) => (
+        <i key={i} className="fa-solid fa-star"></i>
+      ))}
+    </h3>
+  );
 
   const imgCoeurVeganList = [
     "imgCoeurVeganA",
@@ -10,56 +21,92 @@ const ListeBDCards = () => {
     "imgCoeurVeganD",
   ];
 
-  const handlePrevClick = () => {
-    setCurrentImgIndex(
-      (prevIndex) =>
-        (prevIndex - 1 + imgCoeurVeganList.length) % imgCoeurVeganList.length
-    );
+  const imgCochonsList = [
+    "petitscochonspage1",
+    "petitscochonspage2",
+    "petitscochonspage3",
+    "petitscochonspage4",
+    "petitscochonspage5",
+    "petitscochonspage6",
+    "petitscochonspage7",
+    "petitscochonspage8",
+    "petitscochonspage9",
+  ];
+
+  const handlePrevClick = (category, listLength) => {
+    setImgIndices({
+      ...imgIndices,
+      [category]: (imgIndices[category] - 1 + listLength) % listLength,
+    });
   };
 
-  const handleNextClick = () => {
-    setCurrentImgIndex(
-      (prevIndex) => (prevIndex + 1) % imgCoeurVeganList.length
-    );
+  const handleNextClick = (category, listLength) => {
+    setImgIndices({
+      ...imgIndices,
+      [category]: (imgIndices[category] + 1) % listLength,
+    });
   };
+
+  const renderButton = (condition, handleClick, iconClass) =>
+    condition && (
+      <button onClick={handleClick}>
+        <i className={iconClass}></i>
+      </button>
+    );
+
+  const renderImage = (imgList, category) => (
+    <div className="BDCard">
+      <div className={imgList[imgIndices[category]]}></div>
+    </div>
+  );
 
   return (
     <div className="listeBDCards">
+      <div className="BDCard">{renderImage(imgCochonsList, "cochons")}</div>
+      <div className="BDCardMultiple">
+        <div className="buttonprev">
+          {renderButton(
+            imgIndices["cochons"] > 0,
+            () => handlePrevClick("cochons", imgCochonsList.length),
+            "fa-solid fa-square-caret-left"
+          )}
+        </div>
+        <div className="buttonnext">
+          {renderButton(
+            imgIndices["cochons"] < imgCochonsList.length - 1,
+            () => handleNextClick("cochons", imgCochonsList.length),
+            "fa-solid fa-square-caret-right"
+          )}
+        </div>
+      </div>
+      {renderStars()}
+
       <div className="BDCard">
-        {/* Afficher l'image actuelle */}
-        <div className={imgCoeurVeganList[currentImgIndex]}></div>
+        {renderImage(imgCoeurVeganList, "coeurVegan")}
       </div>
       <div className="BDCardMultiple">
-        {/* Afficher le bouton Previous uniquement si ce n'est pas la première image */}
-        {currentImgIndex > 0 && (
-          <button onClick={handlePrevClick}>
-            <i className="fa-solid fa-square-caret-left"></i>
-          </button>
-        )}
-        {/* Afficher le bouton Next uniquement si ce n'est pas la dernière image */}
-        {currentImgIndex < imgCoeurVeganList.length - 1 && (
-          <button onClick={handleNextClick}>
-            <i className="fa-solid fa-square-caret-right"></i>
-          </button>
-        )}
+        <div className="buttonprev">
+          {renderButton(
+            imgIndices["coeurVegan"] > 0,
+            () => handlePrevClick("coeurVegan", imgCoeurVeganList.length),
+            "fa-solid fa-square-caret-left"
+          )}
+        </div>
+        <div className="buttonnext">
+          {renderButton(
+            imgIndices["coeurVegan"] < imgCoeurVeganList.length - 1,
+            () => handleNextClick("coeurVegan", imgCoeurVeganList.length),
+            "fa-solid fa-square-caret-right"
+          )}
+        </div>
       </div>
-      <h3>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-      </h3>
+      {renderStars()}
+
       <div className="BDCard">
         <div className="imgOmnivoreChoix"></div>
       </div>
-      <h3>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-      </h3>
+      {renderStars()}
+
       <div className="BDCard">
         <div className="imgVache"></div>
       </div>
